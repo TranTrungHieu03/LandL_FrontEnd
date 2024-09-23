53c757041b95f6355da75e64fe77d17a2bf8a1b5
hieutt03 <hieuttjob@gmail.com>
2024-09-23 19:37:15 +0700

    fix ui

--- src/components/organisms/MyOrder/ProcessHeader.tsx
+++ src/components/organisms/MyOrder/ProcessHeader.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const ProcessHeader = ({ status }: Props) => {
   return (
-    <div className={'grid md:grid-cols-7 bg-slate-50 mx-10 rounded p-2 sm:grid-cols-1 my-4'}>
+    <div className={'grid md:grid-cols-7 bg-slate-50 mx-10 rounded p-2 my-4'}>
       {processState.map((state, index) => (
         <ProcessState title={state.title} key={index} id={state.id} length={processState.length} status={status} />
       ))}
